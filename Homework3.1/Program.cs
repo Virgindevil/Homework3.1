656b659b312a65fffbb6217817bdeee747f54b51
Virgin <Virgin@DESKTOP-40B4LIG>
2022-01-26 11:11:33 +0400

    Добавьте файлы проекта.

---
+++ Homework3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Введите число для проверки чётное оно или нет:");
+            int number = int.Parse(Console.ReadLine());
+            if (number%2==0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
